d19d273d7fe93205fbd664a5f4d1aa421589020f
placido <placidobb.ac@gmail.com>
2022-06-09 22:51:45 -0300

    Criando novos componentes

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,46 @@
-import React from "react";
-import Link from "../src/components/Link";
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+// import React from "react";
+// import Link from "../src/components/Link";
+// import Footer from "../src/components/patterns/Footer";
 
 
 
 
-function Title({ children, as }) {
-  const Tag = as;
-  return(
-    <React.Fragment>
-      <Tag>
-          {children}
-      </Tag>
+// function Title({ children, as }) {
+//   const Tag = as;
+//   return(
+//     <React.Fragment>
+//       <Tag>
+//           {children}
+//       </Tag>
 
-      <style jsx>{`
-        ${Tag} {
-          color:red;
+//       <style jsx>{`
+//         ${Tag} {
+//           color:red;
           
-        }
-      `}</style>
-    </React.Fragment>
+//         }
+//       `}</style>
+//     </React.Fragment>
    
-  );
-}
-export default function HomePage() {
-  return (
-    <div>
-      <GlobalStyle />
-      <Title as="h2">Alura Cases - Home Page</Title>
-      <Link href="/faq">
-        Ir para a  FAQ
-      </Link>
-      <div>
-      <Link href="/sobre">
-        Ir para a  Sobre
-      </Link>
-      </div>
+//   );
+// }
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h2">Alura Cases - Home Page</Title>
+//       <Link href="/faq">
+//         Ir para a  FAQ
+//       </Link>
+//       <div>
+//       <Link href="/sobre">
+//         Ir para a  Sobre
+//       </Link>
+//       <Footer />
+//       </div>
      
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
